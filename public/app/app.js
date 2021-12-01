ae44d5b14ddbfc22912cc6c7f26c2f642856c3df
jrice <jr16@iu.edu>
2021-12-01 14:04:02 -0500

    recipes page

--- public/app/app.js
+++ public/app/app.js
@@ -23,19 +23,33 @@ function route(){
     //pageID holds page name
 
     if(!pageID){
-        MODEL.changePage("home");    
+        MODEL.changePage("home");   
+       // console.log("what") 
            
     }else{
         MODEL.changePage(pageID);
+        //console.log(pageID)
     }
+}
+ 
+function initlisteners(){
+    $(window).on("hashchange", route);
+    $("div .filters__container__section__button").click(function(){
+        console.log("what")
+        $(this).addClass(" --selected");
+       
+    });
+    
+    route();
 }
 
 
 $(document).ready(function(){
     try{
         initFirebase();
-        route();
-       // initlistener();
+    
+        //route();
+       initlisteners();
        // underlineActivePage();
       //  browseRecipes();
 
