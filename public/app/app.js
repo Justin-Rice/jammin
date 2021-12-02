7f5c25074528a5a1593bd2dedf15acf76e49bc23
jrice <58012439+Justin-Rice@users.noreply.github.com>
2021-12-02 12:13:15 -0500

    recipes additions

--- public/app/app.js
+++ public/app/app.js
@@ -5,18 +5,130 @@ function initFirebase(){
     .auth()
     .onAuthStateChanged(function(user){
         if(user){
-
             loggedIn = true;
+            $("#logOut").css("display","block");
+            $("#logIn").css("display","none");
+
         
 
             console.log("user detected");
         }else{
+            $("#logIn").css("display","block");
+            $("#logOut").css("display","none");
+
+
           
             console.log("user not there");
         }
     })
 
 }
+function logInUser(){
+    let email = $("#email-li").val();
+    let password = $("#pw-li").val();
+    
+   // console.log("login");
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    $("#email-li").val("");
+    $("#pw-li").val("");
+    var user = userCredential.user;
+    Swal.fire({
+        icon: 'success',
+        text: "You have successfully logged in.",
+        background: "#ffd666" ,
+        confirmButtonColor: '#A7E8BD'
+      })
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    Swal.fire({
+        background: "#ffd666",
+        title: "Error",
+        text: 'There has been an unexpected error in Login.',
+        confirmButtonColor: '#F25C54'
+
+      })
+  });
+
+}
+function createUser(){
+    let fName = $("#fName").val();
+    let lName = $("#lName").val();
+    let email = $("#email").val();
+    let password = $("#pw").val();
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+
+    $("#fName").val("");
+    $("#lName").val("");
+    $("#email").val("");
+    $("#pw").val("");
+    var user = userCredential.user;
+    Swal.fire({
+        background: "#ffd666",
+        title: "Account Created",
+        text: 'you have been logged in.',
+        confirmButtonColor: '#A7E8BD'
+
+      })
+
+    //console.log("account created")
+    
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    Swal.fire({
+        background: "#ffd666",
+        title: "Error",
+        text: 'There has been an unexpected error in account creation',
+        confirmButtonColor: '#F25C54'
+
+      })
+  });
+}
+function signOut(){
+
+    Swal.fire({
+        text: "Are you sure you want to log out?",
+        icon: 'warning',
+        background: "#ffd666" ,
+        showCancelButton: true,
+        confirmButtonColor: '#A7E8BD',
+        cancelButtonColor: '#F25C54',
+        confirmButtonText: 'Yes, logout',
+        customClass: {
+            container: 'popup-back',
+            popup: 'popup',
+            header: 'head',
+
+        }
+
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            background: "#ffd666",
+            text: 'You have been logged out.',
+            confirmButtonColor: '#A7E8BD'
+
+          })
+          firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
+          
+        }
+      })
+
+}
+
 function route(){
     let hashTag = window.location.hash;
     let pageID = hashTag.replace("#/","");
@@ -26,19 +138,37 @@ function route(){
         MODEL.changePage("home");   
        // console.log("what") 
            
+    }else if(pageID == 'recipes'){
+        MODEL.changePage("recipes", recipeListeners);
+
     }else{
         MODEL.changePage(pageID);
         //console.log(pageID)
     }
 }
+function recipeListeners(){
+    $(".filters__container__section__button").click(function(){
+        if($(this).hasClass("--selected")){
+            $(this).removeClass("--selected");
+            $(this).addClass("--not-selected");
+
+
+            console.log("yes")
+
+        }else{
+        $(this).addClass("--selected");
+        $(this).removeClass("--not-selected");
+        }
+       
+    });
+}
+function recipeReset(){
+
+}
  
 function initlisteners(){
     $(window).on("hashchange", route);
-    $("div .filters__container__section__button").click(function(){
-        console.log("what")
-        $(this).addClass(" --selected");
-       
-    });
+   
     
     route();
 }
--- public/app/model.js
+++ public/app/model.js
@@ -11,9 +11,43 @@ var MODEL = (function(){
         
     }
 
+    var _loadRecipesPage = function(index){
+        _db
+        .collection("Recipes")
+        .get()
+        .then(function(querySnapshot){
+            querySnapshot.forEach(function(doc){
+               
+               $(".recipes").append(`
+               <div class="album">
+                  <h1>${doc.data().title}</h1>
+                  <div class="cover" style="background-image: url(${doc.data().cover});">
+                </div>
+                <div class="cont">
+                  <p><span>Genre: </span>${doc.data().genre}</p>
+                  <p><span>Artist: </span>${doc.data().artist}</p>
+                </div>
+    
+    
+    
+               </div>
+               `)
+               //console.log(doc.data()) 
+            })
+    
+        }, 
+        function(error){
+            console.log("error", error);
+        });
+    
+       
+
+    }
+
     
     return {
         changePage : _changePage,
+        loadRecipesPage: _loadRecipesPage
 
     }
 })();
