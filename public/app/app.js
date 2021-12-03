0e2445b97624c00dca2dc3d8d19b1455974e0c04
jrice <jr16@iu.edu>
2021-12-03 15:38:26 -0500

    create

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,7 @@
 var loggedIn = false;
 var _db;
+var ingredNum = 2;
+var instructNum = 2;
 
 function initFirebase(){
   _db = firebase.firestore();
@@ -10,13 +12,17 @@ function initFirebase(){
             loggedIn = true;
             $("#logOut").css("display","block");
             $("#logIn").css("display","none");
+            $("#create").css("display","flex");
 
         
 
             console.log("user detected");
         }else{
+          loggedIn = false;
             $("#logIn").css("display","block");
             $("#logOut").css("display","none");
+            $("#create").css("display","none");
+
 
 
           
@@ -120,13 +126,119 @@ function signOut(){
 
           })
           firebase.auth().signOut().then(() => {
-            // Sign-out successful.
+            MODEL.changePage('home');
           }).catch((error) => {
             // An error happened.
           });
           
         }
       })
+
+}
+signInWithProvider = (provider)=>{
+  firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+      Toast.fire({
+          icon: 'success',
+          title: 'user signed in',
+        })
+   /** @type {firebase.auth.OAuthCredential} */
+   var credential = result.credential;
+
+   // This gives you a Google Access Token. You can use it to access the Google API.
+   var token = credential.accessToken;
+   // The signed-in user info.
+   var user = result.user;
+   console.log(user + "user google")
+   
+ }).catch((error) => {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   // The email of the user's account used.
+   var email = error.email;
+   // The firebase.auth.AuthCredential type that was used.
+   var credential = error.credential;
+   // ...
+ });
+
+
+}
+signInGoogle = () =>{
+  //console.log("Gwa")
+  var provider = new firebase.auth.GoogleAuthProvider();
+  signInWithProvider(provider);
+}
+
+function createRecipe(){
+  //var x = document.getElementById("createImg");
+  //var i = x.selectedIndex;
+let ingred = [];
+let ins = [];
+  let imageFile = $("#createfile").prop("files")[0];
+  if(imageFile !=null){
+
+    let storageRef = firebase.storage().ref();
+    let imageFile = $("#createfile").prop("files")[0];
+    const metadata = {contentType: imageFile.type };
+    let name = $("#recipe-name").val();
+    let date = Date.parse(new Date());
+    let type = $("#recipe-type").val();
+    let skill = $("#recipe-skill").val();
+    let time = $("#recipe-time").val();
+    let desc = $("#recipe-desc").val();
+
+    $(".addIng").map(function(){
+      ingred.push($(this).val())
+      // console.log($(this).val());
+    })
+
+    $(".addIns").map(function(){
+      ins.push($(this).val())
+     // console.log($(this).val());
+  
+    })
+     
+    console.log(ingred, ins);
+    console.log("booyah");
+
+    _db = firebase.firestore();
+    const task = storageRef 
+    .child("food/" + date + imageFile.name)
+    .put(imageFile, metadata)
+    task
+    .then((snapshot)=>snapshot.ref.getDownloadURL())
+    .then((url)=>{
+      let userObj = {
+          recipeDesc: desc,
+          recipeIngred : ingred,
+          recipeIns : ins,
+          recipeImageURL: url,
+          recipeNum: date,
+          recipeName: name,
+          recipeType: type,
+          recipeSkill : skill,
+          recipePrep : time,
+      }
+      console.log(url);
+      _db
+      .collection("Recipes")
+      .doc()
+      .set(userObj)
+      .then(function(doc){
+     
+
+      })
+  });
+  
+
+
+  
+  }else{
+    console.log("missing image");
+  }
 
 }
 
@@ -189,6 +301,23 @@ function loadRecipeDescription(index){
 
 function reset(){
   location.reload();
+
+}
+function addIngred(e){
+  $(".ingredients").append(`
+  <input class="addIng" id="ind${ingredNum}" type="text" placeholder="Ingredient #${ingredNum}" />
+  `)
+
+  ingredNum ++;  
+
+}
+
+function addInstruct(e){
+  $(".instructions").append(`
+  <input class="addIns" id="ind${instructNum}" type="text" placeholder="Instruction #${instructNum}" />
+  `)
+
+  instructNum ++;  
 
 }
 
--- public/app/model.js
+++ public/app/model.js
@@ -17,13 +17,21 @@ var MODEL = (function(){
         .get()
         .then(function(querySnapshot){
             querySnapshot.forEach(function(doc){
+         
                
                $(".recipes").append(`
                <div class="recipe" id ="${doc.data().recipeNum}">
       <div class="recipe__head">${doc.data().recipeName}</div>
 
       <div class="recipe__info">
-        <a href = "#/description"><div class="recipe__image" onclick="loadRecipeDescription(${doc.data().recipeNum})"></div></a>
+        <a href ="#/recipes"><div class="recipe__image" style=
+        "background-image:
+        url(${doc.data().recipeImageURL});
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        object-fit: fill;" 
+        onclick="loadRecipeDescription(${doc.data().recipeNum})"></div></a>
         <div class="recipe__info__type">Type: ${doc.data().recipeType}</div>
         <div class="recipe__info__difficulty">Difficulty: ${doc.data().recipeSkill}</div>
         <div class="recipe__info__prep">Prep Time: ${doc.data().recipePrep}</div>
@@ -54,7 +62,13 @@ var MODEL = (function(){
                  console.log("yes")
                  $(".desc").append(`
                  <div class="desc__top">
-                 <div class="desc__top__image">
+                 <div class="desc__top__image" style=
+                 "background-image:
+                 url(${doc.data().recipeImageURL});
+                 background-repeat: no-repeat;
+                 background-position: center;
+                 background-size: cover;
+                 object-fit: fill;">
                    <div class="desc__top__image__title">${doc.data().recipeName}</div>
                  </div>
                  <div class="desc__top__info">
@@ -89,13 +103,56 @@ var MODEL = (function(){
                  </div>
                </div>
                <div class="buttons">
-                 <div class="button">Edit</div>
-                 <div class="button">Delete</div>
+                 <div onclick="editRecipe()" class="button">Edit</div>
+                 <div onclick="deleteRecipe()" class="button">Delete</div>
                </div>  `)                 
                  
                 }else{
                  
-                    $(".recipes").append(`
+                    $(".desc").append(`
+                    
+                 <div class="desc__top">
+                 <div class="desc__top__image"  style=
+                 "background-image:
+                 url(${doc.data().recipeImageURL});
+                 background-repeat: no-repeat;
+                 background-position: center;
+                 background-size: cover;
+                 object-fit: fill;" >
+                   <div class="desc__top__image__title">${doc.data().recipeName}</div>
+                 </div>
+                 <div class="desc__top__info">
+                   <div class="desc__top__info__detail">
+                     Type: <span class="fill">${doc.data().recipeType}</span>
+                   </div>
+                   <div class="desc__top__info__detail">
+                     Difficulty: <span class="fill">${doc.data().recipeSkill}</span>
+                   </div>
+                   <div class="desc__top__info__detail">
+                     Prep Time: <span class="fill">${doc.data().recipePrep}</span>
+                   </div>
+                   <div class="desc__top__info__detail">
+                     Description:
+                     <p>
+                     ${doc.data().recipeDesc}
+                     </p>
+                   </div>
+                 </div>
+               </div>
+               <div class="desc__bottom">
+                 <div class="desc__bottom__ingred">
+                   <h1>Ingredients</h1>
+                   <ul id="desc-ingred">
+                   </ul>
+                 </div>
+                 <div class="desc__bottom__instruct">
+                   <h1>Instructions</h1>
+                   <ol id="desc-ins">
+                    
+                   </ol>
+                 </div>
+               </div>
+                 
                      `)                       
 
                  console.log("no")
